b4acb3799511cf85a10d27206739559c47ee685a
skendall <shannonckendall@gmail.com>
2019-03-02 17:40:28 -0500

    Initial Commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Sausage Burger');
