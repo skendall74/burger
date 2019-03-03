88c879765f21a2b7f16807fb17e0103c81e08134
skendall <shannonckendall@gmail.com>
2019-03-02 20:45:59 -0500

    Burger Push

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,60 +2,42 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/cat.js");
+// Import the model (burger.js) to use its database functions.
+var burgerCon = require("../models/burger");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  cat.all(function(data) {
+router.get("/", function (req, res) {
+  burgerCon.all(function (data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/cats", function(req, res) {
-  cat.create([
-    "name", "sleepy"
-  ], [
-    req.body.name, req.body.sleepy
-  ], function(result) {
+router.post("/api/burger", function (req, res) {
+  burgerCon.create(["burger_name"], [req.body.burger_name], function (result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burger/:id", function (req, res) {
   var condition = "id = " + req.params.id;
-
   console.log("condition", condition);
-
-  cat.update({
-    sleepy: req.body.sleepy
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
+  burgerCon.update({
+    burger_name: req.body.burger_name,
+    devoured: req.body.devoured
+  },
+    condition,
+    function (result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
       res.status(200).end();
+
     }
-  });
+  );
 });
-
-router.delete("/api/cats/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  cat.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-// Export routes for server.js to use.
-module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -5,11 +5,11 @@ USE burgers_db;
 -- Create the table tasks.
 CREATE TABLE burgers
 (
-	id int NOT NULL
-	AUTO_INCREMENT,
-burger_name varchar
+	ID INTEGER(11)
+	AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR
 	(255) NOT NULL,
-devoured boolean,
-PRIMARY KEY
-	(id)
+	devoured BOOLEAN,
+	PRIMARY KEY
+	(ID)
 );
