304146320a22296f60d168828b7191340bc14e7e
skendall <shannonckendall@gmail.com>
2019-03-03 18:00:42 -0500

    Route Fix

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,50 @@
 // Import MySQL connection.
 var connection = require("./connection.js");
 
+var orm = {
+  selectAll: function (table, cb) {
+    var queryString = "SELECT * FROM ";
+    queryString += table;
+    connection.query(queryString, function (err, data) {
+      if (err) throw err;
+      cb(data);
+    });
+  },
+  insertOne: function (table, cols, vals, cb) {
+    var queryString = "INSERT INTO ";
+    queryString += table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+    console.log(vals);
+
+
+    connection.query(queryString, vals, function (err, data) {
+      if (err) {
+        throw err;
+      }
+      cb(data);
+    });
+  },
+  updateOne: function (table, objColVals, condition, cb) {
+    var queryString = "Update " + table;
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, function (err, data) {
+      if (err) throw err;
+      cb(data);
+    });
+  }
+}
+
 function printQuestionMarks(num) {
   var arr = [];
 
@@ -15,66 +59,14 @@ function objToSql(ob) {
   var arr = [];
 
   for (var key in ob) {
+    var value = ob[key];
     if (Object.hasOwnProperty.call(ob, key)) {
-      arr.push(key + "=" + ob[key]);
+      value = "'" + value + "'";
+      arr.push(key + "=" + value);
     }
   }
 
   return arr.toString();
 }
-
-var orm = {
-  all: function (tableInput, cb) {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function (err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-  create: function (table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
-
-    queryString += " (" + cols.toString() + ") ";
-    queryString += "VALUES (" + printQuestionMarks(vals.length) + ") ";
-
-    console.log(queryString);
-
-    connection.query(queryString, vals, function (err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
-  update: function (table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
-    queryString += " SET " + objToSql(objColVals);
-    queryString += " WHERE " + condition;
-
-    console.log(queryString);
-    connection.query(queryString, function (err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
-  delete: function (table, condition, cb) {
-    var queryString = "DELETE FROM " + table;
-    queryString += " WHERE " + condition;
-
-    connection.query(queryString, function (err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  }
-};
 
 module.exports = orm;
--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -16,22 +16,30 @@ router.get("/", function (req, res) {
   });
 });
 
-router.post("/api/burger", function (req, res) {
-  burgers.create(["burger_name"], [req.body.burger_name], function (result) {
+router.post("/", function (req, res) {
+  burgers.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
-  
+
 router.put("/:id", function (req, res) {
   var condition = "id = " + req.params.id;
   console.log("condition", condition)
   
   burgers.update({
     devoured: req.body.devoured
-  }, condition, function()  {
-    res.redirect("/");
-  });
+  }, condition,
+    function (result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+
+    }
+  );
 });
 
+// Export routes for server.js to use.
 module.exports = router;
